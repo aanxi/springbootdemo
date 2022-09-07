51fc55667be2a14cd62ba898b6555151badf40dc
jia-zhang-1 <64758670+jia-zhang-1@users.noreply.github.com>
2022-09-07 16:53:58 +0800

    默认更改列表

---
+++ src/main/java/com/example/springbootdemo/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @ResponseBody
+    @RequestMapping("/test")
+    public String hello() {
+        return "hello Spring Boot!";
+    }
+}
